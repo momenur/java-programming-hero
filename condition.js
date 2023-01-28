15c6b8b2786978028c15918b2daa5963a8d3812f
momenur <momenurislam6@gmail.com>
2023-01-28 16:54:18 +0600

    condition

---
+++ condition.js
@@ -0,0 +1,24 @@
+// var a = 10;
+// var b = 4;
+// if(a > b){
+//     console.log(a);
+// }
+var num1 = 200 ;
+var num2 = 59 ;
+var num3 = 100 ;
+if(num1 > num2){
+    if(num1>num3){
+        console.log(num1);
+    }
+    else{
+        console.log(num3);
+    }
+}
+else{
+    if(num2 > num3){
+        console.log(num2)
+    }
+    else{
+        console.log(num3)
+    }
+}
---
+++ shorthand.js
@@ -0,0 +1,5 @@
+var love = 99;
+love++;
+console.log(love);
+love+=1;
+console.log(love);
