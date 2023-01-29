24a858a6037e1117ed3cd4e309d0b2a100aa3e6d
momenur <momenurislam6@gmail.com>
2023-01-29 10:46:49 +0600

    added array relatives

--- array.js
+++ array.js
@@ -1,1 +1,11 @@
-console.log(6!=6)
+var friendsName = ['Humayon', 'Jahangir', 'Somrat', 'Sakib', 'Shakil', 'Rifat'];
+console.log(friendsName.indexOf('Somrat'));
+console.log(friendsName[5])
+for(i = 0; i < friendsName.length; i++){
+    console.log('The Element of Array' + i + " "  + friendsName[i]);
+}
+console.log(typeof(friendsName));
+var a = function(){
+    console.log('Hellow World');
+}
+console.log(typeof(a));
---
+++ parseint.js
@@ -0,0 +1,15 @@
+var number1 = '33';
+var number2 = parseInt(number1);
+// console.log(typeof(number2));
+
+var number3 = 33 ;
+var number4 = parseFloat(number3);
+// console.log(number4);
+
+var items = [20, 30, 40];
+items[1] = 50;
+items.push(60);
+items.push(70);
+items.push(80);
+items.pop();
+console.log(items.indexOf(20));
