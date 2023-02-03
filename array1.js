5b9e98071250c1ebbe8db10d6d7ed0ef096c5f05
momenur <momenurislam6@gmail.com>
2023-02-03 16:35:34 +0600

    Added Slice and Splice

--- array1.js
+++ array1.js
@@ -6,9 +6,12 @@ const friends = ['rahim', 'karim', 'Jabbar', 'Satter', 'jalil'];
 function sum(a, b){
     return a + b;
 }
-console.log(typeof name);
-console.log(typeof age);
-console.log(typeof isIndependent);
-console.log(typeof student);
-console.log(typeof friends);
-console.log(Array.isArray(friends));
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isIndependent);
+// console.log(typeof student);
+// console.log(typeof friends);
+// console.log(Array.isArray(friends));
+const newFriends = ['HeroAlam', 'NuraFatehi'];
+const allFriends = friends.concat(newFriends);
+console.log(allFriends);
---
+++ slice.js
@@ -0,0 +1,7 @@
+const friends = ['Rahim', 'Karim', 'Humayon', 'Jabbar', 'Ataur', 'Shefali', 'ArjuVhai', 'Mubarak'];
+const firstSlice = friends.slice(2, 5);
+console.log(friends);
+console.log(firstSlice);
+const firstSplice = friends.splice(2, 4);
+console.log(firstSplice);
+console.log(friends);
