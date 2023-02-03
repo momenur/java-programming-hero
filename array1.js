6a7663a4f65261c8ac91408b5bca402ce2236a46
momenur <momenurislam6@gmail.com>
2023-02-03 15:44:25 +0600

    added array

---
+++ array1.js
@@ -0,0 +1,14 @@
+const name = 'BanglaDesh';
+const age = 62;
+const isIndependent = true;
+const student = {name: 'Momen', class: 11, ID: 124,};
+const friends = ['rahim', 'karim', 'Jabbar', 'Satter', 'jalil'];
+function sum(a, b){
+    return a + b;
+}
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isIndependent);
+console.log(typeof student);
+console.log(typeof friends);
+console.log(Array.isArray(friends));
